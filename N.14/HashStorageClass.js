57d13028aeacf21abd6960decda699f8646a4ec1
Sash <tribeca1983@gmail.com>
2021-10-01 13:01:53 +0300

    add class

---
+++ N.14/HashStorageClass.js
@@ -0,0 +1,29 @@
+"use strict";
+class HashStorageClass  {
+  constructor() {
+    this.storage = {};
+  }
+  
+   addValue(key, value) {
+     this.storage[key] = value ;
+   }  
+
+   getValue(key) {
+     return this.storage[key];
+   }
+
+   deleteValue(key) {
+    if ((key in this.storage)) {
+       delete this.storage[key];
+       return true;
+    } else {
+       return false;
+    }
+   }
+
+    getKeys() {
+      return Object.keys(this.storage);
+     
+   }
+
+}

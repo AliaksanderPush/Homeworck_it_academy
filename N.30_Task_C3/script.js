b3384f671f5faf3a65aa3a9f097990612e7d404d
Sash <tribeca1983@gmail.com>
2021-10-26 22:20:02 +0300

    All

--- N.30_Task_C3/script.js
+++ N.30_Task_C3/script.js
@@ -8,9 +8,9 @@ function deepComp(object1, object2) {
               object2 instanceof Object && !Array.isArray(object2)) {
       return  compareObject(object1, object2);
 
-   } else if (typeof object1 === 'number' && isNaN(object1) &&
-              typeof object1 === 'number' && isNaN(object2)) {
-      return true;
+   } else if ( isNaN(object1) && typeof object1 === 'number' &&
+               isNaN(object2) && typeof object2 === 'number') {
+         return true;     
 
    }  else {
       return object1 === object2;
@@ -19,31 +19,13 @@ function deepComp(object1, object2) {
 
  
 
-function compareArray(array1, array2) {
-  if (array1.length !== array2.length) {
-     return false;
+function compareArray(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+       return false;
   } 
-      const arr1 = array1.sort((a,b) => a - b);
-      const arr2 = array2.sort((a,b) => a - b);
-   
       return arr1.every((value, i) => { 
-   
-      if (Array.isArray(value) && Array.isArray(arr2[i])) {
-         return  compareArray(value, arr2[i]);
-
-      } else if ((value instanceof Object  &&  arr2[i] instanceof Object &&
-          value !== null  && arr2[i] !== null)) {
-        return  compareObject(value, arr2[i]);
-
-      } else if ( isNaN(value) && typeof value === 'number' &&
-                  isNaN(arr2[i]) && typeof arr2[i] === 'number') {
-         return true;      
-
-      }  else {
-         return  value === arr2[i]; 
-      } 
-});
- 
+          return  deepComp(value, arr2[i]);
+      }); 
 }
 
 function compareObject(obj1, obj2) {
@@ -61,20 +43,7 @@ function compareObject(obj1, obj2) {
       }
    }
    return keys1.every( key => {
-     if (Array.isArray(obj1[key]) && Array.isArray(obj1[key])) {
-        return compareArray(obj1[key],obj2[key]);
-
-     } else  if (obj1[key] instanceof Object  &&  obj2[key] instanceof Object &&
-          obj1[key] !== null  && obj2[key] !== null) {
-        return compareObject(obj1[key],obj2[key]);
-           
-      } else if ( isNaN(obj1[key]) && typeof obj1[key] === 'number' &&
-                  isNaN(obj2[key]) && typeof obj2[key] === 'number') {
-         return true;      
-
-      } else {
-          return obj1[key] === obj2[key];
-      }
+       return deepComp(obj1[key],obj2[key]);
    });
 }
 

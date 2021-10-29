9b91d0580d51b7d624a7066d868a5ff2ab194baa
Sash <tribeca1983@gmail.com>
2021-10-30 00:35:29 +0300

    done

---
+++ Task_B3+/Test.js
@@ -0,0 +1,49 @@
+const str1 = '0.5*6+(7-3)';
+const str2 = '2*6.5+8/4';
+const str3 = '-2.5*2*(6+4)';
+const str4 = '(8.5-4.5)*(3-1)';
+const str5 = '(-4-6)/2';
+const str6 = '(3+5)/2';
+const str7 = '3+5*(2+4)/(2-1)';
+const str8 = '-2.5+4/2-1';
+const str9 = '10+24/8*(15-5)';
+const str10 = '50-10-5/(2-1)';
+const str11 = '(-20+25)*(-4-3)';
+
+
+
+
+const testObject = [
+    {result:miniCalculator(str1), test:'тест 1.1', res: 7 },
+    {result:miniCalculator(str2), test:'тест 1.2', res: 15},
+    {result:miniCalculator(str3), test:'тест 1.3', res: -50}, 
+    {result:miniCalculator(str4), test:'тест 1.4', res: 8},
+    {result:miniCalculator(str5), test:'тест 1.5', res: -5},
+    {result:miniCalculator(str6), test:'тест 1.6', res: 4},
+    {result:miniCalculator(str7), test:'тест 1.7', res: 33},
+    {result:miniCalculator(str8), test:'тест 1.8', res: -1.5},
+    {result:miniCalculator(str9), test:'тест 1.9', res: 40},
+    {result:miniCalculator(str10), test:'тест 1.10', res: 35},
+    {result:miniCalculator(str11), test:'тест 1.10', res: -35} 
+  ];
+
+ function deepCopyTest(arr) {
+    let count = 0;   
+                                  
+       arr.forEach(elem => {
+       if ((elem.result == elem.res)) {
+         console.log(`${elem.test} : ожидаемый результат ${elem.res} : полученный результат ${elem.result} - ТЕСТ ПРОЙДЕН`);
+       }  else {
+        count++;
+        console.log(`${elem.test} : ожидаемый результат ${elem.res} полученный результат ${elem.result} - ТЕСТ НЕ ПРОЙДЕН!!!!`);
+       }
+    })
+    console.log('Результаты тестирования:')
+    console.log('Пройденных тестов'+':'+(arr.length - count));
+    console.log('Не пройденных тестов'+':'+count);
+    console.log('_________________________________________________________________________________________________')
+    }
+    
+   deepCopyTest(testObject);
+    
+  

538f9886094ffd0cf4fa34c2410821bfdca92669
Sash <tribeca1983@gmail.com>
2021-11-02 14:26:33 +0300

    done

--- N.33/script.js
+++ N.33/script.js
@@ -11,23 +11,37 @@
          electClock = clock.querySelector('.el_clock');
   
    const sizeNumber = radius => radius/4.5;// фун-я,которая возвращает размер цифр взависимости от диаметра часов 
-   const radiusNumber = (radius, sizeNumbers) => radius - sizeNumbers; // диаметр окружности на которой распологаютя кружки для цифр
+   const radiusNumber = (radius, sizeNumbers) => radius - sizeNumbers; // радиус окружности на которой распологаютя кружки для цифр
    const arrowWigth = radius => radius/20; //фун-я которая считает толщину стрелок взависимости от диаметра часов 
-   const numCenter = (radius, sizeNumbers) => radius - sizeNumbers/2; //фун-я определяет положение кружка на цифеблате по координатам   
+   const numCenter = (radius, sizeNumbers) => radius - sizeNumbers/2; //фун-я определяет положение кружка на цифеблате по координатам 
+   const angles = i => i / 12 * Math.PI * 2; // функция дя расчета угла в радианах 
+   const fontSizes = (sizeNumber, k) => sizeNumber / k; //фун-я для расчета размера шрифта с учетом коэфф. k
    const deg = 6,// угол поворота сек/мин стрелки в сек/мин в градусах
          hour = 12, // кол-во часов
          degHour = 30; // угол поворота часовой стрелки в час в градусах
-       
-     
+   const k1 = 1.5, // коэффициенты отвечающие за ширину стрелок часов
+         k2 = 4,
+         k3 = 8,
+         k4 = 2;
+   const n1 = 1.2, // коэффициенты отвечающие за высоту стрелок часов
+         n2 = 1.5, 
+         n3 = 2;    
+   const l1 = 2, // поправочные коэффициенты по координатам Х
+         l2 = 4,
+         l3 = 6;
+   const t1 = 2.5; // поправочны коэф на координату У  
+   const f1 = 1.5, // коэффициенты размеров шрифта
+         f2 = 2;
    btn.addEventListener('click', hideInput);   
    window.addEventListener('load', getTime);
 
    function hideInput() {
-   const  radiusClock = input.value / 2; // диаметр циферблата(часов)
-   if (!radiusClock || radiusClock > 800 || radiusClock < 200) {
+   let  diametrClock = +input.value; // диаметр циферблата(часов)
+   if (!diametrClock || diametrClock > 800 || diametrClock < 200) {
     return  alert('Введите диаметр от 200 до 800 пикселей');
    }  
       div.classList.add('hide');
+      const radiusClock = diametrClock / 2;
       clockBuild(radiusClock);
    }
 
@@ -40,14 +54,14 @@
       circle.classList.add('baground_circle');
       
       // Создаем и размещаем кружки для цифр на циферблате
-      for (let i = 1; i<=hour; i++) {
-         const angle = i / 12 * Math.PI * 2;
+      for (let i = 1; i <= hour; i++) {
+         const angle = angles(i);
          const sizeNumbers = sizeNumber(radiusClock);
          const radiusNumbers = radiusNumber(radiusClock, sizeNumbers);
          const numCenterX = numCenter(radiusClock,sizeNumbers);
          const numCenterY = numCenter(radiusClock,sizeNumbers);
          const numPosX = numCenterX - Math.cos(angle) * radiusNumbers;
-         const numPosY = numCenterY - Math.sin(angle) * radiusNumbers;
+         const numPosY = numCenterY + Math.sin(angle) * radiusNumbers;
          const numCircle = document.createElement('div');
          numCircle.style.width = sizeNumbers + 'px';
          numCircle.style.height = sizeNumbers + 'px';
@@ -55,9 +69,8 @@
          numCircle.style.top = numPosX + 'px';
          numCircle.style.left = numPosY + 'px';
          numCircle.textContent = i;
-         numCircle.classList.add('block_numbers');
          numCircle.style.lineHeight = sizeNumbers + "px"
-         numCircle.style.fontSize = sizeNumbers / 1.5 + "px"
+         numCircle.style.fontSize = sizeNumbers / f1 + "px"
          numCircle.classList.add('numbers_circle');
          circle.append(numCircle);
          clock.append(circle);
@@ -66,35 +79,35 @@
 
      
       const arroWigth  = arrowWigth(radiusClock);
-      hoursArrow.style.width = arroWigth/1.5 + 'px'; 
-      hoursArrow.style.height = radiusClock/2 + 'px'; 
+      hoursArrow.style.width = arroWigth/k1 + 'px'; 
+      hoursArrow.style.height = radiusClock/n3 + 'px'; 
       hoursArrow.style.position = 'absolute';
       hoursArrow.style.bottom = radiusClock +'px';
-      hoursArrow.style.left = radiusClock - arroWigth / 2 + "px"
+      hoursArrow.style.left = radiusClock - arroWigth / l1+ "px"
       hoursArrow.style.zIndex = '2';
       
       
-      minutsArrow.style.width = arroWigth/4 + 'px';
-      minutsArrow.style.height = radiusClock/1.5 + 'px';
+      minutsArrow.style.width = arroWigth/k2 + 'px';
+      minutsArrow.style.height = radiusClock/n2 + 'px';
       minutsArrow.style.position = 'absolute';
       minutsArrow.style.bottom = radiusClock +'px';
-      minutsArrow.style.left = radiusClock - arroWigth / 4 + "px";
+      minutsArrow.style.left = radiusClock - arroWigth / l2 + "px";
       minutsArrow.style.zIndex = '5';
       
       
-      secondsArrow.style.width = arroWigth/8 + 'px';
-      secondsArrow.style.height = radiusClock/1.2 + 'px';
+      secondsArrow.style.width = arroWigth/k3 + 'px';
+      secondsArrow.style.height = radiusClock/n1 + 'px';
       secondsArrow.style.position = 'absolute';
       secondsArrow.style.bottom = radiusClock +'px';
-      secondsArrow.style.left = radiusClock - arroWigth / 6 + "px";
+      secondsArrow.style.left = radiusClock - arroWigth / l3 + "px";
       secondsArrow.style.zIndex = '2';
 
       const sizeNumbers = sizeNumber(radiusClock);
       electClock.style.position = "absolute"; //позиционируем электронные часы
-      electClock.style.left = radiusClock - radiusClock / 4 + "px"; 
-      electClock.style.top = radiusClock / 2.5 + "px"; 
-      electClock.style.width = radiusClock / 2 + "px"; 
-      electClock.style.fontSize = sizeNumbers / 2 + "px"; 
+      electClock.style.left = radiusClock - radiusClock / l2 + "px"; 
+      electClock.style.top = radiusClock / t1 + "px"; 
+      electClock.style.width = radiusClock / k4 + "px"; 
+      electClock.style.fontSize = sizeNumbers / f2 + "px"; 
       electClock.style.zIndex = '3';
       
    }

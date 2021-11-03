c97c1e9dbd834ee245f4313176d55ee616502fbd
Sash <tribeca1983@gmail.com>
2021-11-03 23:40:53 +0300

    then

--- Task_B3+/Test.js
+++ Task_B3+/Test.js
@@ -1,9 +1,9 @@
 const str1 = '0.5*6+(7-3)';
 const str2 = '2*6.5+8/4';
-const str3 = '-2.5*2*(6+4)';
+const str3 = '-2.5*2*(6+4/2)';
 const str4 = '(8.5-4.5)*(3-1)';
-const str5 = '(-4-6)/2';
-const str6 = '(3+5)/2';
+const str5 = '(-4-6)/2+3';
+const str6 = '-(3+5)/2';
 const str7 = '3+5*(2+4)/(2-1)';
 const str8 = '-2.5+4/2-1';
 const str9 = '10+24/8*(15-5)';
@@ -12,21 +12,21 @@ const str11 = '-20-(2+5)';
 const str12 = "2*-(-12+5+4)";
 
 
-/*
 
+/*
 const testObject = [
     {result:miniCalculator(str1), test:'тест 1.1', res: 7 },
     {result:miniCalculator(str2), test:'тест 1.2', res: 15},
-    {result:miniCalculator(str3), test:'тест 1.3', res: -50}, 
+    {result:miniCalculator(str3), test:'тест 1.3', res: -40}, 
     {result:miniCalculator(str4), test:'тест 1.4', res: 8},
-    {result:miniCalculator(str5), test:'тест 1.5', res: -5},
-    {result:miniCalculator(str6), test:'тест 1.6', res: 4},
+    {result:miniCalculator(str5), test:'тест 1.5', res: -2},
+    {result:miniCalculator(str6), test:'тест 1.6', res: -4},
     {result:miniCalculator(str7), test:'тест 1.7', res: 33},
     {result:miniCalculator(str8), test:'тест 1.8', res: -1.5},
     {result:miniCalculator(str9), test:'тест 1.9', res: 40},
     {result:miniCalculator(str10), test:'тест 1.10', res: 35},
     {result:miniCalculator(str11), test:'тест 1.11', res: -27},
-    {result:miniCalculator(str12), test:'тест 1.11', res: 14} 
+    {result:miniCalculator(str12), test:'тест 1.12', res: 6} 
   ];
 
  function deepCopyTest(arr) {

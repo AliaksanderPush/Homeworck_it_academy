e60c2e211e361f45336c5ebc0873a15e3cb35d32
Sash <tribeca1983@gmail.com>
2021-11-15 15:26:01 +0300

    add

--- Task_D1+/script.js
+++ Task_D1+/script.js
@@ -1,4 +1,7 @@
-const el = document.querySelector(".item");
+const el = document.querySelector("img");
+const container = document.querySelector('.container');
+const resizers = document.querySelectorAll(".resizer");
+
 
 let isResizing = false;
 
@@ -14,13 +17,14 @@ function mousedown(e) {
   
   function mousemove(e) {
     if (!isResizing) {
+      container.style.position = 'absolute';
       let newX = prevX - e.clientX;
       let newY = prevY - e.clientY;
 
       const rect = el.getBoundingClientRect();
       
-      el.style.left = rect.left - newX + "px";
-      el.style.top = rect.top - newY + "px";
+      container.style.left = rect.left - newX + "px";
+      container.style.top = rect.top - newY + "px";
 
          prevX = e.clientX;
          prevY = e.clientY;
@@ -28,13 +32,17 @@ function mousedown(e) {
     }
   }
 
+
+
+
   function mouseup() {
+    container.style.position = 'relative';
     window.removeEventListener("mousemove", mousemove);
     window.removeEventListener("mouseup", mouseup);
   }
 }
 
-const resizers = document.querySelectorAll(".resizer");
+
 let currentResizer;
 
 for (let resizer of resizers) {
@@ -53,14 +61,14 @@ for (let resizer of resizers) {
     function mousemove(e) {
       const rect = el.getBoundingClientRect();
 
-      if (currentResizer.classList.contains("se")) {
+      if (currentResizer.classList.contains("topLeft")) {
         el.style.width = rect.width - (prevX - e.clientX) + "px";
         el.style.height = rect.height - (prevY - e.clientY) + "px";
-      } else if (currentResizer.classList.contains("sw")) {
+      } else if (currentResizer.classList.contains("topRight")) {
         el.style.width = rect.width + (prevX - e.clientX) + "px";
         el.style.height = rect.height - (prevY - e.clientY) + "px";
         el.style.left = rect.left - (prevX - e.clientX) + "px";
-      } else if (currentResizer.classList.contains("ne")) {
+      } else if (currentResizer.classList.contains("bottomLeft")) {
         el.style.width = rect.width - (prevX - e.clientX) + "px";
         el.style.height = rect.height + (prevY - e.clientY) + "px";
         el.style.top = rect.top - (prevY - e.clientY) + "px";
@@ -81,5 +89,32 @@ for (let resizer of resizers) {
       isResizing = false;
     }
   }
-  
+
 }
+
+
+function preload() {
+  const content = document.createComment('div');
+  content.classList.add("content");
+  content.style.width = '300px';
+  content.style.height = '150px';
+  content.style.backgroundColor = 'orange';
+
+  const ne = document.createElement('div');
+  ne.style.width = '10px';
+  ne.style.height = '10px';
+  ne.style.backgroundColor = 'black'; 
+  ne.style.position = 'absolute';
+  ne.top = '-1px';
+  ne.left = '-1px';
+
+  const s = document.createElement('div');
+  s.style.width = '10px';
+  s.style.height = '10px';
+  s.style.backgroundColor = 'black'; 
+  s.style.position = 'absolute';
+  s.top = '-1px';
+  s.left = (content.offsetWidth / 2)+'px' ;
+
+}
+preload()

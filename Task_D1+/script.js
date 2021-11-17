1c5b7e4aadab3c25bede504d29b6a71c0cbaff36
Sash <tribeca1983@gmail.com>
2021-11-17 15:17:36 +0300

    re

--- Task_D1+/script.js
+++ Task_D1+/script.js
@@ -2,56 +2,74 @@
 window.addEventListener('load', () => {
 const container = document.querySelector('#container'),
       wrap = document.querySelector('#wrap'),
-      resizers = container.querySelectorAll('.resizer');
-  
-container.addEventListener('mousedown', mousedown);
+      resizers = container.querySelectorAll('.resizer'),
+      bottomMidle = document.querySelector(".bottomMidle"),
+      rightMidle =  document.querySelector(".rightMidle");
+
+  container.addEventListener('mousedown', mousedown);
+
+ 
+
 let isResizing = false;
 
 function mousedown(e) {
   e.preventDefault();
-  isResizing = false;
-  window.addEventListener('mousemove', mousemove);
-  window.addEventListener('mouseup', mouseup);
-    
+ //if (e.target.id === 'container') {
+ 
   let prevX = e.clientX - e.target.getBoundingClientRect().left;
   let prevY = e.clientY - e.target.getBoundingClientRect().top;
 
-function mousemove(e) {
-  e.preventDefault();
-  if (!isResizing) {   
-    let coordX = e.pageX - prevX;
-    let coordY = e.pageY - prevY;
-      let minX = wrap.getBoundingClientRect().left / 2;
-      let minY = wrap.getBoundingClientRect().top / 2;
-      let maxX = wrap.getBoundingClientRect().right - minX;
-      let maxY = wrap.getBoundingClientRect().bottom - minY;
-      if (
-        coordX > minX &&
-        coordX < maxX - e.target.offsetWidth &&
-        coordY > minY &&
-        coordY < maxY - e.target.offsetHeight ) {
-          
-        e.target.style.left = coordX - e.target.parentNode.getBoundingClientRect().left + 'px';
-        e.target.style.top =  coordY - e.target.parentNode.getBoundingClientRect().top + 'px'; 
-      }
-  }  
+  window.addEventListener('mousemove', mousemove);
+  e.target.addEventListener('mouseup', mouseup);
     
-}
-function mouseup() {
-  e.preventDefault();
-  window.removeEventListener('mousemove', mousemove);
-  window.removeEventListener('mouseup', mouseup);
-}
-  
-}
+
+    function mousemove(e) {
+      e.preventDefault();
+     if (!isResizing) {   
+        let coordX = e.clientX - prevX;
+        let coordY = e.clientY - prevY;
+        
+          let wrapleftX = wrap.getBoundingClientRect().left / 2;
+          let wrapRightX = wrap.getBoundingClientRect().right - wrapleftX;
+
+          let wrapTopY = wrap.getBoundingClientRect().top / 2;
+          let wrapBottomY = wrap.getBoundingClientRect().bottom - wrapTopY;
+          
+          
+          if (
+            coordX > wrapleftX &&
+            coordX < wrapRightX - e.target.offsetWidth &&
+            coordY > wrapTopY &&
+            coordY < wrapBottomY - e.target.offsetHeight ) {
+              
+            e.target.style.left = coordX - e.target.parentNode.getBoundingClientRect().left + 'px';
+            e.target.style.top =  coordY - e.target.parentNode.getBoundingClientRect().top + 'px'; 
+          }
+ 
+    } 
+    
+  }
+  function mouseup() {
+    e.preventDefault();
+    window.removeEventListener('mousemove', mousemove);
+    window.removeEventListener('mouseup', mouseup);
+  }
+//}
+           
+ }
+   
+
 
 for (let resizer of resizers) {
   resizer.addEventListener('mousedown', mousedown);
+
   function mousedown(e) {
     e.preventDefault();
+   if (e.which=== 1) {
     isResizing = true;
     const targetDiv = e.target.parentNode;
     let targetDivPos = targetDiv.getBoundingClientRect();
+    let wrapPosition = wrap.getBoundingClientRect();
 
     let startPosX = e.clientX;
     let startPosY = e.clientY;
@@ -60,33 +78,34 @@ for (let resizer of resizers) {
     window.addEventListener('mouseup', mouseup);
 
   function mousemove(e)  {
-    if (e.which=== 1) {
+   
        e.preventDefault();
-      let wrapPosition = wrap.getBoundingClientRect();
+      
       let currentPosX = e.clientX;
       let currentPosY = e.clientY;
+      let resizeValue = 0;
       
       switch (e.target.classList.item(1)) {
         case 'topLeft': {
-           let resizeValue = checkPosition(startPosX, startPosY, currentPosX, currentPosY);
-       
+            resizeValue = checkPosition(startPosX, startPosY, currentPosX, currentPosY);
+            
             targetDiv.style.height = targetDivPos.height - resizeValue + 'px';
             targetDiv.style.width = targetDivPos.width - resizeValue + 'px';
             targetDiv.style.bottom = wrapPosition.bottom - targetDivPos.bottom + 'px';   
             targetDiv.style.right = wrapPosition.right - targetDivPos.right + 'px';     
             targetDiv.style.left = '';
             targetDiv.style.top = '';
-          break;
+            break;
         }
         case 'topMidle': { 
-            let resizeValue = currentPosY - startPosY;
+            resizeValue = currentPosY - startPosY;
             targetDiv.style.height = targetDivPos.height - resizeValue + 'px';
             targetDiv.style.bottom = wrapPosition.bottom - targetDivPos.bottom + 'px';  
             targetDiv.style.top = '';
             break;
           }
-        case 'topRight': {
-            let resizeValue = checkPosition(startPosX, startPosY, currentPosX, currentPosY);
+        case 'topRight': {//+
+            resizeValue = checkPosition(startPosX, startPosY, currentPosX, currentPosY);
             targetDiv.style.height = targetDivPos.height + resizeValue + 'px';
             targetDiv.style.width = targetDivPos.width + resizeValue + 'px';
             targetDiv.style.bottom = wrapPosition.bottom - targetDivPos.bottom + 'px'; 
@@ -94,8 +113,8 @@ for (let resizer of resizers) {
             targetDiv.style.top = '';
             break;  
           }
-        case 'bottomLeft': {   
-            let resizeValue = checkPosition(startPosX, startPosY, currentPosX, currentPosY);
+        case 'bottomLeft': { //+  
+            resizeValue = checkPosition(startPosX, startPosY, currentPosX, currentPosY);
         
             targetDiv.style.height = targetDivPos.height - resizeValue + 'px';
             targetDiv.style.width = targetDivPos.width - resizeValue + 'px';
@@ -104,48 +123,48 @@ for (let resizer of resizers) {
             targetDiv.style.top = targetDivPos.top + 'px';
             break;
           }
-        case 'bottomRight': {  
-            let resizeValue = checkPosition(startPosX, startPosY, currentPosX, currentPosY);
+        case 'bottomRight': {  //+
+            resizeValue = checkPosition(startPosX, startPosY, currentPosX, currentPosY);
             targetDiv.style.height = targetDivPos.height + resizeValue + 'px';
             targetDiv.style.width = targetDivPos.width + resizeValue + 'px'; 
             targetDiv.style.top = targetDivPos.top + 'px';
             targetDiv.style.left = targetDivPos.left + 'px';
             break;
           }   
-         case 'bottomMidle': {  
-            let resizeValue = currentPosY - startPosY;
-            targetDiv.style.height = targetDivPos.height + resizeValue + 'px';
+        case 'bottomMidle': {  
+            resizeValue = currentPosY - startPosY;
+            targetDiv.style.height = targetDivPos.height - resizeValue + 'px';
             targetDiv.style.left = targetDivPos.left + 'px';
             targetDiv.style.top = targetDivPos.top + 'px';
             break;
           }
         case 'rightMidle': {  
-            let resizeValue = currentPosX - startPosX;
-            targetDiv.style.width = targetDivPos.height + resizeValue + 'px';
-            targetDiv.style.left = targetDivPos.left + 'px';
+            resizeValue = currentPosX - startPosX;
+            targetDiv.style.width = targetDivPos.width + resizeValue + 'px';
+            targetDiv.style.left = targetDivPos.left +'px';
             targetDiv.style.top = targetDivPos.top + 'px';
             break;
         }  
-        case 'leftMidle': {
-            let resizeValue = currentPosX - startPosX;
+        case 'leftMidle': { //+
+            resizeValue = currentPosX - startPosX;
             targetDiv.style.width = targetDivPos.width - resizeValue + 'px';
             targetDiv.style.left = '';
             targetDiv.style.right = wrapPosition.right - targetDivPos.right + 'px'; 
             break;
           }
         case "topMidle": {  
-            let resizeValue = currentPosY - startPosY;
+            resizeValue = currentPosY - startPosY;
             targetDiv.style.height = targetDivPos.height + resizeValue + "px";
             targetDiv.style.left = targetDivPos.left + "px";
             targetDiv.style.top = targetDivPos.top + "px";
           break;  
               
       }
-
-
     }
+    // bottomMidle.style.top = 100 - 100 *(bottomMidle.offsetHeight / targetDiv.offsetHeight) + "%";
+    // rightMidle.style.left =   100 - 100 * (rightMidle.offsetWidth / targetDiv.offsetWidth) + "%";
   } 
- }
+}
 function mouseup(e) {
       e.preventDefault();
       document.removeEventListener('mousemove', mousedown);
@@ -154,7 +173,7 @@ function mouseup(e) {
 
 }
 
-}
+
 
 const checkPosition =  (startPosX, startPosY, currentPosX, currentPosY) => {
    if (Math.abs(startPosX + currentPosX) > Math.abs(startPosY - currentPosY)) {
@@ -164,8 +183,9 @@ const checkPosition =  (startPosX, startPosY, currentPosX, currentPosY) => {
       }     
       
 }
-
+}
 });
+
 
 
 
